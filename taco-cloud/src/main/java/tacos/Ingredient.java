677d9c8a4cb0e0460675f187f7d0dfc9977d3f2d
krishna5555 <akshathk55@gmail.com>
2020-11-24 20:52:26 +0530

    controller changes

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -1,17 +1,41 @@
 package tacos;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
 public class Ingredient {
 
-	private String id;
+	public Ingredient(String string, String string2, Type wrap) {
+		// TODO Auto-generated constructor stub
+	}
+
 	private String name;
 	private Type type;
 	
 	public static enum Type{
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
 	}
+	
+	private String id;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
 }
